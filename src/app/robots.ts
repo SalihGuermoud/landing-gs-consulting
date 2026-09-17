21b99474ae058721c45fc48df4d7246523c18542
SalihGuermoud <139645449+SalihGuermoud@users.noreply.github.com>
2026-09-17 14:33:10 +0200

    fix: use canonical domain in robots sitemap URL

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -7,6 +7,6 @@ export default function robots(): MetadataRoute.Robots {
       allow: "/",
       disallow: "/api/",
     },
-    sitemap: "https://www.gs-consulting.fr/sitemap.xml",
+    sitemap: "https://gsconsulting-dev.fr/sitemap.xml",
   };
 }
